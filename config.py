1360e35739de62c4444f65c6c2b679bbd0518bc1
BoreJames <jamestech515@gmail.com>
2025-09-20 00:47:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #Updates Channel t.me/TheSmartDev
 API_ID = https://ivas-sms-panel-api-one.vercel.app/
 API_HASH = "https://ivas-sms-panel-api-one.vercel.app/"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8413701322:AAEidHDmPSKJlMBV_4AH1PdyRoOtRjunyvY"
 COMMAND_PREFIX = [",", ".", "/", "!", "#"]
 UPDATE_CHANNEL_URL = "https://t.me/cybixtech"
 EMAIL = "jadenafrix10@gmail.com"
@@ -10,3 +10,4 @@ PASSWORD = "mahachi2007"
 CHAT_IDS = [-1002665041650]
 
 OWNER_ID = 6524840104
+
